e055aa67b11de1da64784698f405084534399c0c
andriideikalo <wlada801@gmail.com>
2022-12-09 00:10:44 +0200

    add npm

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import './css/styles.css';
+// import './css/styles.css';
 
 import { createMarkup } from './modules/markup';
 import { PER_PAGE, fetchPhotoApi } from './modules/fetch';
@@ -6,9 +6,9 @@ import { simpleGallery } from './modules/simplelightbox';
 import { refs } from './modules/refs';
 
 import {
-  notifyFailure,
-  notifySuccess,
-  notifyInfoSearch,
+    notifyFailure,
+    notifySuccess,
+    notifyInfoSearch,
 } from './modules/notify';
 
 let searchQuery = '';
@@ -18,73 +18,73 @@ let totalHits = '';
 refs.searchForm.addEventListener('submit', onSearch);
 
 async function onSearch(evt) {
-  evt.preventDefault();
-  page = 1;
-  observer.unobserve(refs.guard);
-  searchQuery = evt.currentTarget.searchQuery.value.trim();
-  refs.gallery.innerHTML = '';
-  evt.currentTarget.reset();
-
-  if (!searchQuery) {
+    evt.preventDefault();
+    page = 1;
+    observer.unobserve(refs.guard);
+    searchQuery = evt.currentTarget.searchQuery.value.trim();
     refs.gallery.innerHTML = '';
-    notifyInfoSearch();
-    return;
-  }
+    evt.currentTarget.reset();
 
-  await fetchPhotoApi(searchQuery, page)
-    .then(gallery => {
-      totalHits = gallery.data.totalHits;
+    if (!searchQuery) {
+        refs.gallery.innerHTML = '';
+        notifyInfoSearch();
+        return;
+    }
 
-      if (!totalHits) {
-        return notifyFailure();
-      }
-      notifySuccess(totalHits);
-      addMarkup(gallery.data.hits);
-      simpleGallery.refresh();
+    await fetchPhotoApi(searchQuery, page)
+        .then(gallery => {
+            totalHits = gallery.data.totalHits;
 
-      observer.observe(refs.guard);
-    })
-    .catch(error => console.log(error));
+            if (!totalHits) {
+                return notifyFailure();
+            }
+            notifySuccess(totalHits);
+            addMarkup(gallery.data.hits);
+            simpleGallery.refresh();
+
+            observer.observe(refs.guard);
+        })
+        .catch(error => console.log(error));
 }
 
 function addMarkup(arr) {
-  refs.gallery.insertAdjacentHTML('beforeend', createMarkup(arr));
+    refs.gallery.insertAdjacentHTML('beforeend', createMarkup(arr));
 }
 
 const options = {
-  root: null,
-  rootMargin: '200px',
-  threshold: 1.0,
+    root: null,
+    rootMargin: '200px',
+    threshold: 1.0,
 };
 
 const observer = new IntersectionObserver(onLoad, options);
 
 async function onLoad(entries, observer) {
-  await entries.forEach(entry => {
-    if (entry.isIntersecting) {
-      page += 1;
+    await entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            page += 1;
 
-      fetchPhotoApi(searchQuery, page)
-        .then(gallery => {
-          addMarkup(gallery.data.hits);
-          simpleGallery.refresh();
+            fetchPhotoApi(searchQuery, page)
+                .then(gallery => {
+                    addMarkup(gallery.data.hits);
+                    simpleGallery.refresh();
 
-          const { height: cardHeight } = document
-            .querySelector('.gallery')
-            .firstElementChild.getBoundingClientRect();
+                    const { height: cardHeight } = document
+                        .querySelector('.gallery')
+                        .firstElementChild.getBoundingClientRect();
 
-          window.scrollBy({
-            top: cardHeight * 2,
-            behavior: 'smooth',
-          });
-        })
-        .catch(error => {
-          console.log(error);
-        });
-      if (page === Math.round(totalHits / PER_PAGE)) {
-        observer.unobserve(refs.guard);
-        page = 1;
-      }
-    }
-  });
+                    window.scrollBy({
+                        top: cardHeight * 2,
+                        behavior: 'smooth',
+                    });
+                })
+                .catch(error => {
+                    console.log(error);
+                });
+            if (page === Math.round(totalHits / PER_PAGE)) {
+                observer.unobserve(refs.guard);
+                page = 1;
+            }
+        }
+    });
 }
