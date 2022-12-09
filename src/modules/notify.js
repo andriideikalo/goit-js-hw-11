e1264c577875795d79136346a8bcf4ade7898e88
andriideikalo <wlada801@gmail.com>
2022-12-09 17:31:51 +0200

    fix task

--- src/modules/notify.js
+++ src/modules/notify.js
@@ -1,20 +1,30 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 export const notifyFailure = () => {
-  return Notify.failure(
-    'Sorry, there are no images matching your search query. Please try again.',
-    {
-      timeout: 1000,
-    }
-  );
+    return Notify.failure(
+        'Sorry, there are no images matching your search query. Please try again.', {
+            timeout: 3000,
+            width: '360px',
+            svgSize: '120px',
+            fontSize: '18px',
+        }
+    );
 };
 
 export const notifySuccess = totalHits => {
-  return Notify.success(`Hooray! We found ${totalHits} images.`, {
-    timeout: 1000,
-  });
+    return Notify.success(`Hooray! We found ${totalHits} images.`, {
+        timeout: 3000,
+        width: '360px',
+        svgSize: '120px',
+        fontSize: '18px',
+    });
 };
 
 export const notifyInfoSearch = () => {
-  return Notify.info('Please, fill out this field!', { timeout: 1000 });
+    return Notify.info('Please, fill out this field!', {
+        timeout: 3000,
+        width: '360px',
+        svgSize: '120px',
+        fontSize: '18px',
+    });
 };
